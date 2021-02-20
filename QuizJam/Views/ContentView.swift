8106c95bdcf1ce4b248fa62a023b2fee7bccd092
Sandra E Quel <sandra.quel0714@gmail.com>
2021-02-20 14:39:13 -0500

    Added navigation links

--- QuizJam/Views/ContentView.swift
+++ QuizJam/Views/ContentView.swift
@@ -14,63 +14,51 @@ struct ContentView: View {
             ZStack {
                 Color.white.edgesIgnoringSafeArea(.all)
                 VStack {
+                    NavigationLink(destination: CardList()) {
                     HStack(spacing: 25, content: {
-                        Button(action: {
-                            print("Practice button was tapped")
-                        }) {
+                        NavigationLink(destination: Text("Practice")){
                             ButtonTextUI(buttonText: "Practice")
                                 .foregroundColor(Color.snow)
                                 .background(Color.orangeRed)
                                 .cornerRadius(25)
                         }
-                        Button(action: {
-                            print("Manage button was tapped")
-                        }) {
+                        NavigationLink(destination: CardList()) {
+
                             ButtonTextUI(buttonText: "Manage")
                                 .foregroundColor(Color.snow)
                                 .background(Color.oceanBlue)
                                 .cornerRadius(25)
                         }
                     })
-                    
-                    //Sample Palettes
-                    Spacer()
-                    Section(header: Text("Palette 1")) {
-                        Text("SSSS").background(Color.snow)
-                            .foregroundColor(Color.black)
-                        Text("orangeRed").background(Color.orangeRed)
-                            .foregroundColor(Color.snow)
-                        
-                        Text("maximumYellow").background(Color.maximumYellow)
-                            .foregroundColor(Color.snow)
-                        
-                        Text("oceanBlue").background(Color.oceanBlue)
-                            .foregroundColor(Color.snow)
-                        
-                        Text("carribeanOcean").background(Color.carribeanOcean)
-                            .foregroundColor(Color.snow)
-                        
-                        Text("blizzardBlue").background(Color.blizzardBlue)
-                            .foregroundColor(Color.black)
+
+//                    //Sample Palettes
+//                    Spacer()
+//                    Section(header: Text("Palette 1")) {
+//                        Text("SSSS").background(Color.snow)
+//                            .foregroundColor(Color.black)
+//                        Text("orangeRed").background(Color.orangeRed)
+//                            .foregroundColor(Color.snow)
+//
+//                        Text("maximumYellow").background(Color.maximumYellow)
+//                            .foregroundColor(Color.snow)
+//
+//                        Text("oceanBlue").background(Color.oceanBlue)
+//                            .foregroundColor(Color.snow)
+//
+//                        Text("carribeanOcean").background(Color.carribeanOcean)
+//                            .foregroundColor(Color.snow)
+//
+//                        Text("blizzardBlue").background(Color.blizzardBlue)
+//                            .foregroundColor(Color.black)
+//                    }
+//                    Spacer()
+
                     }
                     Spacer()
-                    
-                    Section(header: Text("Palette 2")) {
-                        Text("cream").background(Color.cream)
-                            .foregroundColor(Color.black)
-                        Text("burntSienna").background(Color.burntSienna)
-                            .foregroundColor(Color.white)
-                        Text("eggShell").background(Color.eggShell)
-                            .foregroundColor(Color.white)
-                        Text("slate").background(Color.slate)
-                            .foregroundColor(Color.white)
-                        Text("bush").background(Color.bush)
-                            .foregroundColor(Color.white)
-                    }
-                }
             }
+        }
             .navigationTitle("QuizJam")
-            navigationBarTitleTextColor(Color.maximumYellow)
+           // navigationBarTitleTextColor(Color.maximumYellow)
         }
     }
     
